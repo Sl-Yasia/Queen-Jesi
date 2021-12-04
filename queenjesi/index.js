e9ca39758d8fbe7964d91f5ad39e8d850b7046dd
yasiya <85439062+Sl-Yasia@users.noreply.github.com>
2021-12-04 15:25:19 +0530

    👑━☽【♥️ 𝓠𝓾𝓮𝓮𝓷 𝓙𝓮𝓼𝓲 ♥️】☾━👑

---
+++ queenjesi/index.js
@@ -0,0 +1,16 @@
+/* Copyright (C) 2021 Sl-Yasia.
+
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+
+Queen-Jesi - Sl-Yasia
+*/
+
+module.exports = {
+    Base: require('./Base'),
+    Message: require('./Message'),
+    StringSession: require('./StringSession'),
+    ReplyMessage: require('./ReplyMessage'),
+    Image: require('./Image'),
+    Video: require('./Video')
+};
