fd6947027b902b390093ce86c05f0d29d6bcd555
yasiya <85439062+Sl-Yasia@users.noreply.github.com>
2022-01-29 12:48:35 +0530

    💞━☽【♥️ 𝓠𝓾𝓮𝓮𝓷 𝓙𝓮𝓼𝓲 ♥️】☾━💞

--- plugins/spamkickb.js
+++ plugins/spamkickb.js
@@ -23,10 +23,10 @@ let baseURI = '/apps/' + config.HEROKU.APP_NAME;
         l_dsc = 'Spam Kick Only Group Command'
         alr_on = '*Spam Kick Already Open!*'
         alr_off = '*Spam Kick is Currently Closed!*'
-        BGM_on = '*Spam Kick Open Successfully 🛡*️'
+        BGM_on = '*Spam Kick Open Successfully 🛡️*'
         BGM_off = '*Spam Kick Close Successfully ♥️*'
     }
-    Jesi.adCmd({pattern: '.skick ?(.*)', fromMe: true, desc: l_dsc, usage: '.skick on / off' }, (async (message, match) => {
+    Jesi.adCmd({pattern: 'skick ?(.*)', fromMe: true, desc: l_dsc, usage: '.skick on / off' }, (async (message, match) => {
         if (match[1] == 'off') {
                 await heroku.patch(baseURI + '/config-vars', { 
                     body: { 
